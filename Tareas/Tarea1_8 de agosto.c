b598315e6436088e01d900ff281187e0363c93f0
Rubi200415 <pr.hernandezfloreano@ugto.mx>
2025-08-12 10:18:30 -0600

    Add files via upload

---
+++ Tareas/Tarea1_8 de agosto.c
@@ -0,0 +1,28 @@
+//8 de agosto del 2025
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+  float Numeros[5] = {1.2,3.5,5.1,7.2,9.5};
+  int i;
+
+  printf ("Números: ")
+  for (i=0; i<5; i++){
+    printf ("%.3f", Numeros[i]);
+  }
+
+  printf("\n\nResultados de las funciones\n");
+    printf("x        x^2       log(x+1)    exp(x)+cos(x)\n");
+
+    for (i = 0; i < 5; i++) {
+        float x = Numeros[i];
+        float cuadrado = x * x;
+        float logaritmo = log(x + 1);
+        float exp_cos = exp(x) + cos(x);
+
+        printf("%.3f   %.3f     %.3f      %.3f\n", x, cuadrado, logaritmo, exp_cos);
+    }
+
+    return 0;
+}
