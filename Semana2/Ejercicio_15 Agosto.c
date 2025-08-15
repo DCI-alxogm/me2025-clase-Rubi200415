2998544ae44d4e0298026620b42e8833e6d7bb04
Rubi200415 <pr.hernandezfloreano@ugto.mx>
2025-08-15 11:59:24 -0600

    Add files via upload

---
+++ Semana2/Ejercicio_15 Agosto.c
@@ -0,0 +1,17 @@
+#include <studio.h>
+#include <math>
+
+int main (){
+    float x1,x2,x3,a,b,c;
+    int d=1;
+
+    if (d!=1){
+
+    printf("Ingrese los valores correspondiemtes de ");
+
+    } else return 0;
+
+
+
+}
+
